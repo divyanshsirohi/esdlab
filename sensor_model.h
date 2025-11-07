30ab95a60a08ff879e058d6f8e20d3cdef87bee4
Divyansh Sirohi <148632808+divyanshsirohi@users.noreply.github.com>
2025-11-07 11:34:50 +0530

    Add files via upload

---
+++ sensor_model.h
@@ -0,0 +1,348 @@
+
+
+
+    // !!! This file is generated using emlearn !!!
+
+    #include <stdint.h>
+    
+
+static inline float sensor_model_tree_0(const int16_t *features, int32_t features_length) {
+          if (features[0] < 41) {
+              if (features[0] < 28) {
+                  if (features[0] < 23) {
+                      if (features[0] < 18) {
+                          return 62.213333f;
+                      } else {
+                          return 78.072993f;
+                      }
+                  } else {
+                      if (features[1] < 29) {
+                          return 88.875000f;
+                      } else {
+                          return 116.644444f;
+                      }
+                  }
+              } else {
+                  if (features[0] < 36) {
+                      if (features[2] < 75) {
+                          return 140.096774f;
+                      } else {
+                          return 171.362832f;
+                      }
+                  } else {
+                      if (features[1] < 32) {
+                          return 200.623188f;
+                      } else {
+                          return 255.333333f;
+                      }
+                  }
+              }
+          } else {
+              if (features[0] < 53) {
+                  if (features[1] < 34) {
+                      if (features[1] < 31) {
+                          return 232.440000f;
+                      } else {
+                          return 276.827273f;
+                      }
+                  } else {
+                      if (features[0] < 45) {
+                          return 310.000000f;
+                      } else {
+                          return 349.400000f;
+                      }
+                  }
+              } else {
+                  if (features[1] < 34) {
+                      if (features[0] < 71) {
+                          return 341.566038f;
+                      } else {
+                          return 424.625000f;
+                      }
+                  } else {
+                      if (features[1] < 34) {
+                          return 411.166667f;
+                      } else {
+                          return 445.538462f;
+                      }
+                  }
+              }
+          }
+        }
+        
+
+static inline float sensor_model_tree_1(const int16_t *features, int32_t features_length) {
+          if (features[0] < 40) {
+              if (features[0] < 27) {
+                  if (features[0] < 21) {
+                      if (features[1] < 31) {
+                          return 67.715190f;
+                      } else {
+                          return 98.833333f;
+                      }
+                  } else {
+                      if (features[1] < 31) {
+                          return 89.488189f;
+                      } else {
+                          return 168.000000f;
+                      }
+                  }
+              } else {
+                  if (features[0] < 33) {
+                      if (features[2] < 81) {
+                          return 147.833333f;
+                      } else {
+                          return 177.866667f;
+                      }
+                  } else {
+                      if (features[1] < 32) {
+                          return 194.232323f;
+                      } else {
+                          return 257.500000f;
+                      }
+                  }
+              }
+          } else {
+              if (features[0] < 60) {
+                  if (features[1] < 33) {
+                      if (features[0] < 44) {
+                          return 240.127660f;
+                      } else {
+                          return 284.685393f;
+                      }
+                  } else {
+                      if (features[1] < 35) {
+                          return 329.716981f;
+                      } else {
+                          return 407.333333f;
+                      }
+                  }
+              } else {
+                  if (features[1] < 32) {
+                      if (features[1] < 32) {
+                          return 295.000000f;
+                      } else {
+                          return 350.000000f;
+                      }
+                  } else {
+                      if (features[1] < 35) {
+                          return 419.020000f;
+                      } else {
+                          return 460.538462f;
+                      }
+                  }
+              }
+          }
+        }
+        
+
+static inline float sensor_model_tree_2(const int16_t *features, int32_t features_length) {
+          if (features[0] < 40) {
+              if (features[0] < 27) {
+                  if (features[0] < 20) {
+                      if (features[1] < 31) {
+                          return 64.705128f;
+                      } else {
+                          return 107.750000f;
+                      }
+                  } else {
+                      if (features[1] < 30) {
+                          return 86.390244f;
+                      } else {
+                          return 110.677419f;
+                      }
+                  }
+              } else {
+                  if (features[0] < 33) {
+                      if (features[1] < 29) {
+                          return 118.000000f;
+                      } else {
+                          return 158.646341f;
+                      }
+                  } else {
+                      if (features[2] < 86) {
+                          return 188.046729f;
+                      } else {
+                          return 229.176471f;
+                      }
+                  }
+              }
+          } else {
+              if (features[0] < 59) {
+                  if (features[1] < 34) {
+                      if (features[0] < 49) {
+                          return 258.877551f;
+                      } else {
+                          return 306.375000f;
+                      }
+                  } else {
+                      if (features[1] < 35) {
+                          return 349.294118f;
+                      } else {
+                          return 439.750000f;
+                      }
+                  }
+              } else {
+                  if (features[1] < 34) {
+                      if (features[1] < 32) {
+                          return 295.000000f;
+                      } else {
+                          return 411.945946f;
+                      }
+                  } else {
+                      if (features[1] < 35) {
+                          return 444.037037f;
+                      } else {
+                          return 480.000000f;
+                      }
+                  }
+              }
+          }
+        }
+        
+
+static inline float sensor_model_tree_3(const int16_t *features, int32_t features_length) {
+          if (features[0] < 37) {
+              if (features[0] < 27) {
+                  if (features[0] < 20) {
+                      if (features[1] < 30) {
+                          return 64.593750f;
+                      } else {
+                          return 87.636364f;
+                      }
+                  } else {
+                      if (features[1] < 31) {
+                          return 88.656000f;
+                      } else {
+                          return 116.111111f;
+                      }
+                  }
+              } else {
+                  if (features[0] < 31) {
+                      if (features[1] < 30) {
+                          return 130.589744f;
+                      } else {
+                          return 163.689655f;
+                      }
+                  } else {
+                      if (features[2] < 88) {
+                          return 181.964912f;
+                      } else {
+                          return 221.153846f;
+                      }
+                  }
+              }
+          } else {
+              if (features[0] < 54) {
+                  if (features[1] < 34) {
+                      if (features[1] < 31) {
+                          return 213.928571f;
+                      } else {
+                          return 270.686131f;
+                      }
+                  } else {
+                      if (features[0] < 45) {
+                          return 299.500000f;
+                      } else {
+                          return 362.750000f;
+                      }
+                  }
+              } else {
+                  if (features[1] < 34) {
+                      if (features[2] < 87) {
+                          return 319.500000f;
+                      } else {
+                          return 381.575758f;
+                      }
+                  } else {
+                      if (features[0] < 60) {
+                          return 393.863636f;
+                      } else {
+                          return 442.235294f;
+                      }
+                  }
+              }
+          }
+        }
+        
+
+static inline float sensor_model_tree_4(const int16_t *features, int32_t features_length) {
+          if (features[0] < 38) {
+              if (features[0] < 27) {
+                  if (features[0] < 21) {
+                      if (features[1] < 30) {
+                          return 64.111888f;
+                      } else {
+                          return 82.937500f;
+                      }
+                  } else {
+                      if (features[0] < 25) {
+                          return 88.255102f;
+                      } else {
+                          return 115.233333f;
+                      }
+                  }
+              } else {
+                  if (features[0] < 31) {
+                      if (features[2] < 77) {
+                          return 120.800000f;
+                      } else {
+                          return 160.722222f;
+                      }
+                  } else {
+                      if (features[0] < 36) {
+                          return 180.733333f;
+                      } else {
+                          return 212.034483f;
+                      }
+                  }
+              }
+          } else {
+              if (features[0] < 54) {
+                  if (features[1] < 31) {
+                      if (features[0] < 44) {
+                          return 201.611111f;
+                      } else {
+                          return 242.000000f;
+                      }
+                  } else {
+                      if (features[1] < 34) {
+                          return 268.842857f;
+                      } else {
+                          return 346.333333f;
+                      }
+                  }
+              } else {
+                  if (features[1] < 33) {
+                      if (features[2] < 83) {
+                          return 284.777778f;
+                      } else {
+                          return 349.714286f;
+                      }
+                  } else {
+                      if (features[1] < 34) {
+                          return 400.632653f;
+                      } else {
+                          return 446.506849f;
+                      }
+                  }
+              }
+          }
+        }
+        
+
+static const uint8_t sensor_model_leaves[316] = { 116, 218, 120, 66, 95, 37, 156, 66, 0, 192, 177, 66, 245, 73, 233, 66, 198, 24, 12, 67, 227, 92, 43, 67, 137, 159, 72, 67, 85, 85, 127, 67, 164, 112, 104, 67, 228, 105, 138, 67, 0, 0, 155, 67, 51, 179, 174, 67, 116, 200, 170, 67, 0, 80, 212, 67, 85, 149, 205, 67, 236, 196, 222, 67, 45, 110, 135, 66, 171, 170, 197, 66, 244, 249, 178, 66, 0, 0, 40, 67, 85, 213, 19, 67, 222, 221, 49, 67, 122, 59, 66, 67, 0, 192, 128, 67, 174, 32, 112, 67, 187, 87, 142, 67, 198, 219, 164, 67, 171, 170, 203, 67, 0, 128, 147, 67, 0, 0, 175, 67, 143, 130, 209, 67, 236, 68, 230, 67, 7, 105, 129, 66, 0, 128, 215, 66, 206, 199, 172, 66, 215, 90, 221, 66, 0, 0, 236, 66, 119, 165, 30, 67, 246, 11, 60, 67, 45, 45, 101, 67, 84, 112, 129, 67, 0, 48, 153, 67, 166, 165, 174, 67, 0, 224, 219, 67, 21, 249, 205, 67, 190, 4, 222, 67, 0, 0, 240, 67, 0, 48, 129, 66, 209, 69, 175, 66, 223, 79, 177, 66, 228, 56, 232, 66, 249, 150, 2, 67, 141, 176, 35, 67, 4, 247, 53, 67, 98, 39, 93, 67, 183, 237, 85, 67, 211, 87, 135, 67, 0, 192, 149, 67, 0, 96, 181, 67, 0, 192, 159, 67, 178, 201, 190, 67, 140, 238, 196, 67, 30, 30, 221, 67, 73, 57, 128, 66, 0, 224, 165, 66, 157, 130, 176, 66, 119, 119, 230, 66, 154, 153, 241, 66, 228, 184, 32, 67, 188, 187, 52, 67, 212, 8, 84, 67, 114, 156, 73, 67, 0, 0, 114, 67, 227, 107, 134, 67, 171, 42, 173, 67, 142, 99, 142, 67, 110, 219, 174, 67, 251, 80, 200, 67, 224, 64, 223, 67 };
+
+float sensor_model_predict(const int16_t *features, int32_t features_length) {
+
+        float avg = 0;
+
+        avg += sensor_model_tree_0(features, features_length); 
+    avg += sensor_model_tree_1(features, features_length); 
+    avg += sensor_model_tree_2(features, features_length); 
+    avg += sensor_model_tree_3(features, features_length); 
+    avg += sensor_model_tree_4(features, features_length); 
+        
+        return avg/5;
+    }
+    
